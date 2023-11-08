2114999923e2a7b41aa469f6668a0b9c5eb38c2a
Lewis Quinn <lewis.quinn@sainsburysbank.co.uk>
2023-11-08 10:31:09 +0000

    FE-002: format api response text

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React, { useState, useEffect } from
- 
-'react';
-import
- 
-'./App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 const App = () => {
   const [data, setData] = useState([]);
 
+  // this address can be changed to get any API
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
+    fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((json) => setData(json));
   }, []);
 
   return (
-    <div
- 
-className="App">
+    <div className="App">
       <h1>Dashboard</h1>
+
+      {/* this list gets content from API */}
       <ul>
         {data.map((post) => (
-          <li key={post.id}>{post.title}</li>
+          <>
+          {/* change key to display desired data from api */}
+            <h3 key={post.id}>{post.title}</h3>
+            <p key={post.id}>{post.body}</p>
+          </>
         ))}
       </ul>
     </div>
