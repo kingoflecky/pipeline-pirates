83b1e06a2c23505f97b0b4a64294d137f1cc498a
Lewis Quinn <lewis.quinn@sainsburysbank.co.uk>
2023-11-08 11:22:59 +0000

    FE-002: added grid format, title & body now neat, SB font brought in

--- src/App.js
+++ src/App.js
@@ -16,12 +16,14 @@ const App = () => {
       <h1>Dashboard</h1>
 
       {/* this list gets content from API */}
-      <ul>
+      <ul className="dashboard-wrapper">
         {data.map((post) => (
           <>
-          {/* change key to display desired data from api */}
-            <h3 key={post.id}>{post.title}</h3>
-            <p key={post.id}>{post.body}</p>
+            {/* change key to display desired data from api */}
+            <div className="response-wrapper">
+              <h3 key={post.id}>{post.title}</h3>
+              <p key={post.id}>{post.body}</p>
+            </div>
           </>
         ))}
       </ul>
