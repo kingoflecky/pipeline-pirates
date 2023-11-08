7f789b1d8f7eacb4fdd43ee688952a046cc95284
Lewis Quinn <lewis.quinn@sainsburysbank.co.uk>
2023-11-08 15:35:37 +0000

    FE-003/added-mock-login-screen

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import './App.css'
+import Header from "./components/Header";
 
 const App = () => {
-  const [data, setData] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  // this address can be changed to get any API
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((json) => setData(json));
+    // Check if the user is logged in by checking for a username in localStorage
+    const username = localStorage.getItem("username");
+    if (username) {
+      setIsLoggedIn(true);
+    }
   }, []);
 
   return (
     <div className="App">
-      <h1>Dashboard</h1>
-
-      {/* this list gets content from API */}
-      <ul className="dashboard-wrapper">
-        {data.map((post) => (
-          <>
-            {/* change key to display desired data from api */}
-            <div className="response-wrapper">
-              <h3 key={post.id}>{post.title}</h3>
-              <p key={post.id}>{post.body}</p>
-            </div>
-          </>
-        ))}
-      </ul>
+      {isLoggedIn ? (
+        <>
+          <Header setIsLoggedIn={setIsLoggedIn} />
+          <Dashboard />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 };
