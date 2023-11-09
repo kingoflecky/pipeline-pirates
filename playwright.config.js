53a791e4a286054758a0b41eb2c86656bed9b9fd
Scott McKenzie <scottmckenzie09@googlemail.com>
2023-11-09 11:36:04 +0000

    updated actions

--- playwright.config.js
+++ playwright.config.js
@@ -70,19 +70,13 @@ module.exports = defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: [
-    {
-      command: 'npm run backend',
-      url: 'http://127.0.0.1:3001',
-      timeout: 120 * 1000,
-      reuseExistingServer: !process.env.CI,
-    },
-    {
-      command: 'npm run frontend',
-      url: 'http://127.0.0.1:3000',
-      timeout: 120 * 1000,
-      reuseExistingServer: !process.env.CI,
-    }
-  ],
+  // webServer: 
+  //   {
+  //     command: 'npm run start',
+  //     url: 'http://127.0.0.1:3001',
+  //     timeout: 120 * 1000,
+  //     reuseExistingServer: !process.env.CI,
+  //   },
+
 });
 
