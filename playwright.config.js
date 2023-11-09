e94aca9962410ad12df5a79da2bd5bd29ac7defa
Scott McKenzie <scottmckenzie09@googlemail.com>
2023-11-09 11:17:40 +0000

    more tweaks

--- playwright.config.js
+++ playwright.config.js
@@ -70,12 +70,19 @@ module.exports = defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: 
+  webServer: [
     {
-      command: 'npm run start',
+      command: 'npm run backend',
       url: 'http://127.0.0.1:3001',
       timeout: 120 * 1000,
       reuseExistingServer: !process.env.CI,
     },
+    {
+      command: 'npm run frontend',
+      url: 'http://127.0.0.1:3000',
+      timeout: 120 * 1000,
+      reuseExistingServer: !process.env.CI,
+    }
+  ],
 });
 
