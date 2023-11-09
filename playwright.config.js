36276d45add6d66d464c76acbce0fc72a40fe1b4
Scott McKenzie <scottmckenzie09@googlemail.com>
2023-11-09 11:14:11 +0000

    tweaks

--- playwright.config.js
+++ playwright.config.js
@@ -73,11 +73,9 @@ module.exports = defineConfig({
   webServer: 
     {
       command: 'npm run start',
-      url: 'http://127.0.0.1:3000',
+      url: 'http://127.0.0.1:3001',
       timeout: 120 * 1000,
       reuseExistingServer: !process.env.CI,
     },
-
-  
 });
 
