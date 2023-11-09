2ef03bcc39ebd77241bc537d65c2592b42fff01d
Andrea Stevenson <andrea.stevenson@sainsburysbank.co.uk>
2023-11-09 08:25:01 +0000

    added header test

--- tests/dashboard.page/dashboard.spec.js
+++ tests/dashboard.page/dashboard.spec.js
@@ -2,11 +2,14 @@
 const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }, testInfo) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('http://localhost:3000/dashboard');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/SB Dashboard/);
 
+  //Expect Header to be visible
+  await expect(page.getByRole('heading', { name: 'Welcome, please sign in' })).toBeVisible();
+  
   // Attach screenshot to HTML report
   const screenshot = await page.screenshot();
   await testInfo.attach('screenshot', { body: screenshot, contentType: 'image/png' });
